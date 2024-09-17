4e9b7a061135a543fbcf81ce114cfcff7985f79f
Neale <nealetham@tamu.edu>
2024-09-17 19:29:54 -0400

    fix: validate major model composite key uniqueness

--- app/models/major.rb
+++ app/models/major.rb
@@ -3,6 +3,7 @@
 class Major < ApplicationRecord
   # Validations
   validates :mname, :cname, presence: true
+  validates :mname, uniqueness: { scope: :cname }
 
   # Degree requirement associations
   has_and_belongs_to_many :courses
