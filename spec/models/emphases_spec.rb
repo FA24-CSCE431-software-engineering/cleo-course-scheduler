e1ba3d697c5c1161e2f1eceaf781e42fdc0b6843
Neale <nealetham@tamu.edu>
2024-09-17 23:55:11 -0400

    test: add emphasis model creation test

---
+++ spec/models/emphases_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe Emphasis, type: :emphases do
+    before(:each) do
+        @default_course = Course.create(crn: 1, cname: "Software Engineering", credit_hours: 3)
+    end
+
+    context "When creating a valid emphasis" do
+        it "is valid with valid attributes" do
+            emphasis = Emphasis.create(crn: 1, ename: "Economics")
+            expect(emphasis).to be_valid
+        end
+    end
+
+    context "When creating an invalid emphasis" do
+        it "is invalid with invalid attributes" do
+            emphasis = Emphasis.create(crn: 1, ename: "")
+            expect(emphasis).to be_invalid
+        end
+    end
+
+    context "When creating a duplicate emphasis" do
+        it "is invalid with duplicate crn, ename" do
+            e1 = Emphasis.create(crn: 1, ename: "Economics")
+            e2 = Emphasis.new(crn: 1, ename: "Economics")
+            expect(e2).to be_invalid
+        end
+    end
+
+    context "When creating two emphasis" do
+        it "is valid with unique crn, ename" do
+            e1 = Emphasis.create(crn: 1, ename: "Economics")
+            e2 = Emphasis.create(crn: 1, ename: "Math")
+            expect(e2).to be_valid
+        end
+    end
+end
