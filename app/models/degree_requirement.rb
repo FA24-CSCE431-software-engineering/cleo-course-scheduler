27afea82d2baeda68d4b663cfa9d6c7da1ef5950
Neale <nealetham@tamu.edu>
2024-09-18 00:24:57 -0400

    fix: validate degree req model attribute presence

--- app/models/degree_requirement.rb
+++ app/models/degree_requirement.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class DegreeRequirement < ApplicationRecord
+    validates :course_id, presence: true
+    validates :major_id, presence: true
+
     validates :course_id, uniqueness: { scope: :major_id }
 end
