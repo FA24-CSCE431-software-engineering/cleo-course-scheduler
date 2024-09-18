03bac6f6c48d58b91ddb8b580010f3d82770d191
Neale <nealetham@tamu.edu>
2024-09-17 23:59:33 -0400

    test: add core categories model creation test

---
+++ spec/models/core_categories_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe CoreCategories, type: :core_categories do
+    before(:each) do
+        @default_course = Course.create(crn: 1, cname: "Software Engineering", credit_hours: 3)
+    end
+
+    context "When creating a valid category" do
+        it "is valid with valid attributes" do
+            category = CoreCategories.create(crn: 1, cname: "Communication")
+            expect(category).to be_valid
+        end
+    end
+
+    context "When creating an invalid category" do
+        it "is invalid with invalid attributes" do
+            category = CoreCategories.create(crn: 1, cname: "")
+            expect(category).to be_invalid
+        end
+    end
+
+    context "When creating a duplicate category" do
+        it "is invalid with duplicate crn, cname" do
+            c1 = CoreCategories.create(crn: 1, cname: "Communication")
+            c2 = CoreCategories.new(crn: 1, cname: "Communication")
+            expect(c2).to be_invalid
+        end
+    end
+
+    context "When creating two category" do
+        it "is valid with unique crn, cname" do
+            c1 = CoreCategories.create(crn: 1, cname: "Communication")
+            c2 = CoreCategories.create(crn: 1, cname: "Mathematics")
+            expect(c2).to be_valid
+        end
+    end
+end
