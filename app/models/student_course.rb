f7a9184a31ab35875a250f2c44410700c51d6927
Maria Viteri <marviteri03@gmail.com>
2024-09-21 21:53:50 -0500

    Finished delete for student, all CRUD operations functional

--- app/models/student_course.rb
+++ app/models/student_course.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 class StudentCourse < ApplicationRecord
+  # student added to fix delete issue
+  belongs_to :student
+  belongs_to :course
+
   validates :student_id, presence: true
   validates :course_id, presence: true
-
   validates :course_id, uniqueness: { scope: :student_id }
 end
