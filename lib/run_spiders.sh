978f2b04142ab90069c6d959b71b21b44bdfe94a
Neale <nealetham@tamu.edu>
2024-10-01 23:27:23 -0400

    chore: script to scrape majors

--- lib/run_spiders.sh
+++ lib/run_spiders.sh
@@ -3,10 +3,7 @@
 folder_name="data"
 
 # Attempt to create the folder
-if mkdir "$folder_name" 2>/dev/null; then
-    echo "Folder '$folder_name' created successfully."
-else
-    echo "Folder '$folder_name' already exists."
-fi
+mkdir "$folder_name" 2>/dev/null
 
 scrapy runspider ./scripts/course_spider.py --nolog &
+scrapy runspider ./scripts/major_spider.py --nolog &
