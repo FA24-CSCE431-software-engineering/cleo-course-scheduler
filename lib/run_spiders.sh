812a6a6cf8a29c44eb3b812c799445fcc6e07f6c
Neale <nealetham@tamu.edu>
2024-10-01 23:29:45 -0400

    chore: script to scrape core categories

--- lib/run_spiders.sh
+++ lib/run_spiders.sh
@@ -7,3 +7,4 @@ mkdir "$folder_name" 2>/dev/null
 
 scrapy runspider ./scripts/course_spider.py --nolog &
 scrapy runspider ./scripts/major_spider.py --nolog &
+scrapy runspider ./scripts/core_category_spider.py --nolog &
