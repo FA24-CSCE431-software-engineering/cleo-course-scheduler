77bd036835947feb717e21830c1d33c04cb81945
Neale <nealetham@tamu.edu>
2024-10-02 19:05:04 -0400

    fix: name error from refactoring

--- spec/models/core_categories_spec.rb
+++ spec/models/core_categories_spec.rb
@@ -2,25 +2,25 @@
 
 require 'rails_helper'
 
-RSpec.describe CoreCategories, type: :core_categories do
+RSpec.describe CoreCategory, type: :core_categories do
   context 'When creating a valid category' do
     it 'is valid with valid attributes' do
-      c = CoreCategories.create(cname: 'Communication')
+      c = CoreCategory.create(cname: 'Communication')
       expect(c).to be_valid
     end
   end
 
   context 'When creating an invalid category' do
     it 'is invalid with missing cname' do
-      c = CoreCategories.create(cname: '')
+      c = CoreCategory.create(cname: '')
       expect(c).to be_invalid
     end
   end
 
   context 'When creating a duplicate category' do
     it 'is invalid with duplicate cname' do
-      CoreCategories.create(cname: 'Communication')
-      c1 = CoreCategories.new(cname: 'Communication')
+      CoreCategory.create(cname: 'Communication')
+      c1 = CoreCategory.new(cname: 'Communication')
       expect(c1).to be_invalid
     end
   end
