b7f264e606867d5a4054fe589c7f47aa1efec28f
Neale <nealetham@tamu.edu>
2024-10-02 14:37:37 -0400

    chore: rubocop linting

--- db/migrate/20241002155951_recreate_tracks.rb
+++ db/migrate/20241002155951_recreate_tracks.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RecreateTracks < ActiveRecord::Migration[7.2]
   def change
     drop_table :tracks
--- spec/models/track_spec.rb
+++ spec/models/track_spec.rb
@@ -3,17 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe Track, type: :track do
-  
   context 'When creating a valid track' do
     it 'is valid with valid attributes' do
-      track = Track.create(tname: "Software")
+      track = Track.create(tname: 'Software')
       expect(track).to be_valid
     end
   end
 
   context 'When creating an invalid track' do
     it 'is invalid with missing tname' do
-      track = Track.create(tname: "")
+      track = Track.create(tname: '')
       expect(track).to be_invalid
     end
   end
