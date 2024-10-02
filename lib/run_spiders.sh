863d7d214f01b945ac3f1e99e6cdaede1f8c6d6f
Neale <nealetham@tamu.edu>
2024-10-01 23:33:05 -0400

    chore: script to scrape core courses

--- lib/run_spiders.sh
+++ lib/run_spiders.sh
@@ -8,3 +8,4 @@ mkdir "$folder_name" 2>/dev/null
 scrapy runspider ./scripts/course_spider.py --nolog &
 scrapy runspider ./scripts/major_spider.py --nolog &
 scrapy runspider ./scripts/core_category_spider.py --nolog &
+scrapy runspider ./scripts/core_course_spider.py --nolog &
