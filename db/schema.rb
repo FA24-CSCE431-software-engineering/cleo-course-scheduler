49c9952253fdfbea0b5e189a971660f14be06525
Neale <nealetham@tamu.edu>
2024-10-03 12:02:52 -0400

    feat: recreate prereq table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_03_132239) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_03_135610) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -93,11 +93,12 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_03_132239) do
   end
 
   create_table "prerequisites", id: false, force: :cascade do |t|
-    t.integer "course_id"
-    t.integer "prereq_id"
+    t.bigint "course_id", null: false
+    t.bigint "prereq_id", null: false
+    t.integer "equi_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["course_id", "prereq_id"], name: "index_prerequisites_on_course_id_and_prereq_id", unique: true
+    t.index ["course_id", "prereq_id", "equi_id"], name: "index_prerequisites_on_course_id_and_prereq_id_and_equi_id", unique: true
     t.index ["course_id"], name: "index_prerequisites_on_course_id"
     t.index ["prereq_id"], name: "index_prerequisites_on_prereq_id"
   end
@@ -152,6 +153,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_03_132239) do
   add_foreign_key "course_tracks", "tracks"
   add_foreign_key "degree_requirements", "courses"
   add_foreign_key "degree_requirements", "majors"
+  add_foreign_key "prerequisites", "courses"
+  add_foreign_key "prerequisites", "courses", column: "prereq_id"
   add_foreign_key "student_courses", "courses"
   add_foreign_key "student_courses", "students", primary_key: "uin"
   add_foreign_key "students", "majors"
