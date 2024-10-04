e6ec61b996402e1c68487df98c72eb02e7fbc25c
Neale <nealetham@tamu.edu>
2024-10-04 17:20:08 -0400

    feat: allow for more flexibility in adding courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,10 +2,9 @@
 
 class Course < ApplicationRecord
   # Validations
-  validates :cname, :ccode, :cnumber, presence: true
-  validates :credit_hours, presence: true
+  validates :ccode, :cnumber, presence: true
   validates :credit_hours, :lecture_hours, :lab_hours, :cnumber, numericality: { only_integer: true }
-  validates :ccode, length: { is: 4 }
+  validates :ccode, length: { minimum: 4, maximimum: 30 }
   validates :ccode, uniqueness: { scope: :cnumber }
 
   # Prerequisites associations
