82f6f8c89bc1349826aaae6e710989520dbd4cd9
vincentamu <123021332+vincentamu@users.noreply.github.com>
2024-10-15 09:21:44 -0500

    Update 20241015073032_fix_student_emphasis_id.rb

--- db/migrate/20241015073032_fix_student_emphasis_id.rb
+++ db/migrate/20241015073032_fix_student_emphasis_id.rb
@@ -1,5 +1,5 @@
 class FixStudentEmphasisId < ActiveRecord::Migration[7.2]
   def change
-    #rename_column :students, :emphases_id, :emphasis_id
+    rename_column :students, :emphases_id, :emphasis_id
   end
 end
