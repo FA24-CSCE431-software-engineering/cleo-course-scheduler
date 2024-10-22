3b266e58a7bea0f6767ce88535a3731bfa5e3a08
Neale <nealetham@tamu.edu>
2024-10-22 16:10:24 -0400

    test: add shared context for admin logins

--- spec/support/shared_context.rb
+++ spec/support/shared_context.rb
@@ -1,7 +1,15 @@
 RSpec.shared_context 'logged in student', :shared_context => :metadata do
-  include StudentLoginHelper
+  include UserLoginHelper
 
   before(:each) do
     mock_current_student_login
   end 
+end
+
+RSpec.shared_context 'logged in admin', :shared_context => :metadata do
+  include UserLoginHelper
+
+  before(:each) do
+    mock_current_student_login_admin
+  end
 end
