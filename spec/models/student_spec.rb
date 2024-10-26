da6b0dd6f9793851a8bac8bab260de9cf5e2bf40
Neale <nealetham@tamu.edu>
2024-10-26 17:50:08 -0400

    test: validate enrol/grad years and alphanumeric for names

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -31,12 +31,44 @@ RSpec.describe Student, type: :model do
   context 'When creating an invalid student' do
     it 'is not valid with missing email' do
       invalid_student = Student.create(
-        google_id: student.google_id,
+        google_id: 1,
         first_name: 'Jack',
         last_name: 'Adams',
         major: student.major,
         enrol_year: 2020,
         grad_year: 2024,
+        enrol_semester: 0,
+        grad_semester: 1
+      )
+      expect(invalid_student).to be_invalid
+    end
+  end
+
+  context 'When creating an invalid student' do
+    it 'is not valid with non alphanumeric characters in name' do
+      invalid_student = Student.create(
+        google_id: 1,
+        first_name: 'Jack!',
+        last_name: 'Adams',
+        major: student.major,
+        enrol_year: 2020,
+        grad_year: 2024,
+        enrol_semester: 0,
+        grad_semester: 1
+      )
+      expect(invalid_student).to be_invalid
+    end
+  end
+
+  context 'When creating an invalid student' do
+    it 'is not valid with enrol_year greater than grad_year' do
+      invalid_student = Student.create(
+        google_id: 1,
+        first_name: 'Jack',
+        last_name: 'Adams',
+        major: student.major,
+        enrol_year: 2024,
+        grad_year: 2020,
         enrol_semester: 0,
         grad_semester: 1
       )
