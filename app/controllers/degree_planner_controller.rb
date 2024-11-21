e4db1535d864094f9e869fa81003251a41ec7702
Uzma Hamid <uzmahamid@Uzmas-MacBook-Air.local>
2024-11-20 23:59:53 -0600

    support page styling added for both admin and students

--- app/controllers/degree_planner_controller.rb
+++ app/controllers/degree_planner_controller.rb
@@ -61,6 +61,8 @@ class DegreePlannerController < ApplicationController
       flash[:error] = "Invalid course ID or semester value: Course ID - #{selected_course_id}, Semester - #{semester}"
     end
 
+    
+
     redirect_to student_degree_planner_path(@student)
   end
 
